1a07796755a4a6a7e04712c70a57584def93a57d
AftabUfaq <engr.aftabufaq@gmail.com>
2024-03-15 10:56:31 +0500

    i have added some new cchanges and made it more clear for you.

--- App.js
+++ App.js
@@ -6,11 +6,20 @@ import {
   Platform,
   SafeAreaView,
   StyleSheet,
+  Text,
+  ToastAndroid,
   View,
 } from 'react-native';
-import {PERMISSIONS, requestMultiple} from 'react-native-permissions';
+import {
+  PERMISSIONS,
+  checkMultiple,
+  openSettings,
+  request,
+  requestMultiple,
+} from 'react-native-permissions';
 import BackgroundService from 'react-native-background-actions';
 import notifee from '@notifee/react-native';
+import CustomButton from './src/components/CustomButton';
 Geolocation.setRNConfiguration({
   skipPermissionRequests: false,
   authorizationLevel: 'always',
@@ -20,32 +29,9 @@ Geolocation.setRNConfiguration({
 
 function App() {
   const [locationAccess, setLocationAccess] = useState(false);
-
-  async function onDisplayNotification() {
-    // Request permissions (required for iOS)
-    await notifee.requestPermission();
-
-    // Create a channel (required for Android)
-    const channelId = await notifee.createChannel({
-      id: 'default',
-      name: 'Default Channel',
-    });
-
-    // Display a notification
-    await notifee.displayNotification({
-      title: 'Notification Title',
-      body: 'Main body content of the notification',
-      android: {
-        channelId,
-        smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
-        // pressAction is needed if you want the notification to open the app when pressed
-        pressAction: {
-          id: 'default',
-        },
-      },
-    });
-  }
-
+  const [batteryOptimizationEnabled1, setBatteryOptimizationEnabled1] =
+    useState(true);
+  const [powerinfo, setPowerInfo] = useState(true);
   const sleep = time =>
     new Promise(resolve => setTimeout(() => resolve(), time));
 
@@ -88,27 +74,107 @@ function App() {
       {enableHighAccuracy: false, timeout: 5000, maximumAge: 10000},
     );
   };
-  const requestLocationAccess = () => {
+
+  const requestCoarseLocationAccess = () => {
     if (Platform.OS == 'android') {
-      requestMultiple([
-        PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION,
-        PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
-        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
-      ]).then(statuses => {
-        if (
-          statuses['android.permission.ACCESS_BACKGROUND_LOCATION'] ==
-            'granted' &&
-          statuses['android.permission.ACCESS_COARSE_LOCATION'] == 'granted' &&
-          statuses['android.permission.ACCESS_FINE_LOCATION']
-        ) {
-          setLocationAccess(true);
+      request(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION).then(status => {
+        if (status == 'granted') {
+          checkAllPermissions();
+          ToastAndroid.showWithGravityAndOffset(
+            'Coarse Location Access has been granted',
+            ToastAndroid.LONG,
+            ToastAndroid.BOTTOM,
+            25,
+            50,
+          );
         } else {
-          setLocationAccess(false);
+          Alert.alert(
+            'Permission Denied',
+            'To run the app pleaase open setting and allow location permission',
+            [
+              {
+                text: 'Open Setting',
+                onPress: () => openSettings(),
+              },
+            ],
+          );
         }
       });
     }
   };
 
+  const requestBgLocationAccess = () => {
+    if (Platform.OS == 'android') {
+      request(PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION).then(status => {
+        if (status == 'granted') {
+          checkAllPermissions();
+          ToastAndroid.showWithGravityAndOffset(
+            'Bg Location Access has been granted',
+            ToastAndroid.LONG,
+            ToastAndroid.BOTTOM,
+            25,
+            50,
+          );
+        } else {
+          Alert.alert(
+            'Permission Denied',
+            'To run the app pleaase open setting and allow location permission',
+            [
+              {
+                text: 'Open Setting',
+                onPress: () => openSettings(),
+              },
+            ],
+          );
+        }
+      });
+    }
+  };
+
+  const requestFineLocationAccess = () => {
+    if (Platform.OS == 'android') {
+      request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then(status => {
+        if (status == 'granted') {
+          checkAllPermissions();
+          ToastAndroid.showWithGravityAndOffset(
+            'Fine Location Access has been granted',
+            ToastAndroid.LONG,
+            ToastAndroid.BOTTOM,
+            25,
+            50,
+          );
+        } else {
+          Alert.alert(
+            'Permission Denied',
+            'To run the app pleaase open setting and allow location permission',
+            [
+              {
+                text: 'Open Setting',
+                onPress: () => openSettings(),
+              },
+            ],
+          );
+        }
+      });
+    }
+  };
+
+  const checkAllPermissions = () => {
+    checkMultiple([
+      PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION,
+      PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
+    ]).then(statuses => {
+      if (
+        statuses['android.permission.ACCESS_BACKGROUND_LOCATION'] ==
+          'granted' &&
+        statuses['android.permission.ACCESS_COARSE_LOCATION'] &&
+        statuses['android.permission.ACCESS_FINE_LOCATION'] == 'granted'
+      ) {
+        setLocationAccess(true);
+      }
+    });
+  };
   const startBgService = async () => {
     await BackgroundService.start(veryIntensiveTask, options);
     await BackgroundService.updateNotification({
@@ -123,11 +189,12 @@ function App() {
   const checkBatteryOptimizationTechniques = async () => {
     const batteryOptimizationEnabled =
       await notifee.isBatteryOptimizationEnabled();
+    setBatteryOptimizationEnabled1(batteryOptimizationEnabled);
     if (batteryOptimizationEnabled) {
       // 2. ask your users to disable the feature
       Alert.alert(
         'Restrictions Detected',
-        'To ensure notifications are delivered, please disable battery optimization for the app.',
+        'To ensure app run in background, please disable battery optimization for the app.',
         [
           // 3. launch intent to navigate the user to the appropriate screen
           {
@@ -148,27 +215,29 @@ function App() {
 
   const powerManagerInfo = async () => {
     const powerManagerInfo = await notifee.getPowerManagerInfo();
+    
+    console.log(powerManagerInfo);
     if (powerManagerInfo.activity) {
       // 2. ask your users to adjust their settings
       Alert.alert(
-          'Restrictions Detected',
-          'To ensure notifications are delivered, please adjust your settings to prevent the app from being killed',
-          [
-            // 3. launch intent to navigate the user to the appropriate screen
-            {
-              text: 'OK, open settings',
-              onPress: async () => await notifee.openPowerManagerSettings(),
-            },
-            {
-              text: "Cancel",
-              onPress: () => console.log("Cancel Pressed"),
-              style: "cancel"
-            },
-          ],
-          { cancelable: false }
-        );
-    };
-  }
+        'Restrictions Detected',
+        'To ensure notifications are delivered, please adjust your settings to prevent the app from being killed',
+        [
+          // 3. launch intent to navigate the user to the appropriate screen
+          {
+            text: 'OK, open settings',
+            onPress: async () => await notifee.openPowerManagerSettings(),
+          },
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+        ],
+        {cancelable: false},
+      );
+    }
+  };
   return (
     <SafeAreaView
       style={{
@@ -177,33 +246,91 @@ function App() {
       }}>
       {locationAccess ? (
         <View>
-          <Button title="Start Bg service" onPress={() => startBgService()} />
-          <View style={{height: 30}} />
-          <Button title="Stop Bg service" onPress={() => stopBgService()} />
-          <View style={{height: 30}} />
+          <Text
+            style={{
+              padding: 15,
+              fontWeight: '700',
+              textAlign: 'justify',
+              fontSize: 16,
+            }}>
+            To ensure that the background service starts successfully, it's
+            essential to enable battery optimization and power options for the
+            app. Otherwise, the operating system might terminate your app.
+          </Text>
+          {batteryOptimizationEnabled1 && (
+            <CustomButton
+              label={
+                'Battery Optimization help to keep app live in backgroud and will be killed closed by OS. you can read more about this here . '
+              }
+              link={
+                'https://notifee.app/react-native/docs/android/background-restrictions'
+              }
+              text="Open Battery Optimization Setting"
+              onPress={() => checkBatteryOptimizationTechniques()}
+            />
+          )}
+
+          {powerinfo && (
+            <CustomButton
+              label={
+                'Power Manager help to keep app live and can help you to restart app if it has been closed by OS. you can read more about this here . '
+              }
+              link={
+                'https://notifee.app/react-native/docs/android/background-restrictions'
+              }
+              text="Open Power Manager setting"
+              onPress={() => powerManagerInfo()}
+            />
+          )}
+
+          <CustomButton
+            label={
+              "To ensure that the background service starts successfully, it's essential to enable battery optimization and power options for the app. Otherwise, the operating system might terminate your app."
+            }
+            text="Start Backgroud Service"
+            onPress={() => startBgService()}
+          />
+
+          <CustomButton
+            label={'You can stop the service from here'}
+            text="Stop Backgroud Service"
+            onPress={() => stopBgService()}
+          />
         </View>
       ) : (
-        <Button
-          title="Request Location Access"
-          onPress={() => requestLocationAccess()}
-        />
+        <View>
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 16,
+              textAlign: 'justify',
+              padding: 15,
+            }}>
+            The Android operating system provides three types of location
+            access: Fine Location and Coarse Location. Fine Location and Coarse
+            Location are used in the foreground. If you require access to
+            location services in the background, you'll need to obtain
+            permission for this. To get background permission, you may need to
+            open the app's settings on your Android device and enable the
+            necessary permission from there.
+          </Text>
+          <CustomButton
+            label={'Click Here to Get Backgroud Location Accesss'}
+            text="Request Background Location"
+            onPress={() => requestBgLocationAccess()}
+          />
+          <CustomButton
+            label={'Click Here to Get Fine Location Accesss'}
+            text="Request Fine Location"
+            onPress={() => requestFineLocationAccess()}
+          />
+          <CustomButton
+            label={'Click Here to Get Coarse Location Accesss'}
+            text="Request Coarse Location"
+            onPress={() => requestCoarseLocationAccess()}
+          />
+        </View>
       )}
-      <View style={{height: 30}} />
-      <Button
-        title="Display Notification"
-        onPress={() => onDisplayNotification()}
-      />
-      <View style={{height: 30}} />
-      <Button
-        title="Batttery OPtimization"
-        onPress={() => checkBatteryOptimizationTechniques()}
-      />
-      <View style={{height: 30}} />
-      
-     <Button
-        title="Power Manager Info"
-        onPress={() => powerManagerInfo()}
-      />
     </SafeAreaView>
   );
 }
